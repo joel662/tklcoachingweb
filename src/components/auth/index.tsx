c9cbeb88732083a8ca8362940225e3d978b6097a
Ben Geo Abraham <bengeoabraham@gmail.com>
2024-07-26 10:35:39 +0530

    added auth

---
+++ src/components/auth/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { authModalState } from '../../atoms/authModalAtom';
+
+
+type AuthPageProps  = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps > = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default AuthPage ;
